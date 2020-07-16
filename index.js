2972380853bb6eb59e7282a0befd82129b0bd92b
Brian St. Amand <brian@cto.ai>
2020-07-16 00:05:51 -0700

    authd http reqs working

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const fetch = require('fetch').fetchUrl;
+const fetch = require('node-fetch');
 
 try {
+
   const stage = core.getInput('stage');
   const status = core.getInput('status');
   const team_id = core.getInput('team_id');
   const custom = core.getInput('custom');
   const change_id = core.getInput('change_id');
   const pipeline_id = core.getInput('pipeline_id');
-  console.log(stage,status,team_id,custom,change_id,pipeline_id);
-  fetch("http://www.example.com", function(e,m,b) {
-    console.log(body.toString());
-  });
+  const token = core.getInput('token');
+
+  const body = {
+    change_id,
+    custom,
+    pipeline_id,
+    stage,
+    status,
+    team_id
+  };
+  const opts = {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${token}`,
+			'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  };
+  fetch("https://api.cto.sh/api/v1/events", opts)
+    .then(res => res.json())
+    .then(json => console.log(json))
 
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const event_payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log('------ event payload -------------------------------------------------');
+  console.log(`${event_payload}`);
 
 } catch (error) {
   core.setFailed(error.message);
