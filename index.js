4f0bed9b6b6cfb340cd9ee02d5f7db7a738c8598
Brian St. Amand <brian@cto.ai>
2020-07-15 21:04:26 -0700

    update

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fetch = require('fetch').fetchUrl;
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  const stage = core.getInput('stage');
+  const status = core.getInput('status');
+  const team_id = core.getInput('team_id');
+  const custom = core.getInput('custom');
+  const change_id = core.getInput('change_id');
+  const pipeline_id = core.getInput('pipeline_id');
+  console.log(stage,status,team_id,custom,change_id,pipeline_id);
+  await fetch("http://www.example.com", function(e,m,b) {
+    console.log("body.toString()");
+  });
+
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+
 } catch (error) {
   core.setFailed(error.message);
 }
