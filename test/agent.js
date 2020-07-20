e47875774fd1f7d51135497644ce6407d7487eea
Brian St. Amand <brian@cto.ai>
2020-07-20 03:15:51 -0700

    tests

--- test/agent.js
+++ test/agent.js
@@ -18,7 +18,91 @@ const fetch = () => {
   return new Promise((success) => success(JSON.stringify(out)));
 };
 
-const test_extract_body = (function test_extract_body() {
+// passthru
+const test_extractBody_passthrough = (function test_extractBody_passthrough() {
+  const github_context = {
+    context: {
+      payload: {
+        action: "unknown_action",
+        pull_request: {
+          base: {
+            ref: "master",
+          },
+          head: {
+            ref: "arbitary-branch",
+          },
+        },
+      },
+      eventName: "arbitrary_event",
+      ref: "master",
+    },
+  };
+  const actual = extractBody("team-id-123", github_context);
+  const expected = {
+    stage: "arbitrary_event",
+    status: "unknown_action",
+    change_id: "arbitary-branch",
+    team_id: "team-id-123",
+    custom: {
+      context: {
+        payload: {
+          action: "unknown_action",
+          pull_request: {
+            base: { ref: "master" },
+            head: { ref: "arbitary-branch" },
+          },
+        },
+        eventName: "arbitrary_event",
+        ref: "master",
+      },
+    },
+  };
+  assert.deepStrictEqual(actual, expected);
+  console.log("[ OK ]", arguments.callee.name);
+})();
+
+// pr closed
+const test_extractBody_pr_closed = (function test_extractBody_pr_closed() {
+  const github_context = {
+    context: {
+      payload: {
+        action: "closed",
+        pull_request: {
+          base: {
+            ref: "master",
+          },
+          head: {
+            ref: "branch1",
+          },
+        },
+      },
+      eventName: "pull_request",
+      ref: "master",
+    },
+  };
+  const actual = extractBody("team-id-123", github_context);
+  const expected = {
+    stage: "Change",
+    status: "Succeeded",
+    change_id: "branch1",
+    team_id: "team-id-123",
+    custom: {
+      context: {
+        payload: {
+          action: "closed",
+          pull_request: { base: { ref: "master" }, head: { ref: "branch1" } },
+        },
+        eventName: "pull_request",
+        ref: "master",
+      },
+    },
+  };
+  assert.deepStrictEqual(actual, expected);
+  console.log("[ OK ]", arguments.callee.name);
+})();
+
+// pr opened
+const test_extractBody = (function test_extractBody() {
   const github_context = {
     context: {
       payload: {
@@ -61,7 +145,7 @@ const test_extract_body = (function test_extract_body() {
 // When user supplies no input, GitHub supplies empty/null/undef values for various
 // params. These empty params need to be detected and then the GitHub object
 // values used instead to infer the event info.
-const test_construct_body_pr_closed = (function test_construct_body_pr_closed() {
+const test_constructBody_pr_closed = (function test_constructBody_pr_closed() {
   const github_pull_request_closed = {
     context: {
       payload: {
@@ -119,7 +203,7 @@ const test_construct_body_pr_closed = (function test_construct_body_pr_closed() 
 })();
 
 // when the user supplies no input, resulting in empty params
-const test_construct_body_pr_opened = (function test_construct_body_pr_opened() {
+const test_constructBody_pr_opened = (function test_constructBody_pr_opened() {
   const github_pull_request_opened = {
     context: {
       payload: {
@@ -177,7 +261,7 @@ const test_construct_body_pr_opened = (function test_construct_body_pr_opened() 
 
 // Even when given the PR opened metadata from GitHub,
 // the user supplied data should overwrite it.
-const test_construct_body_user_action = (function test_construct_body_user_action() {
+const test_constructBody_user_action = (function test_constructBody_user_action() {
   const github_pull_request_opened = {
     context: {
       payload: {
